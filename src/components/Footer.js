040dddb25cd6eda78340bcf4ede88c24e1402549
itssigron <arel1234123@gmail.com>
2022-06-30 03:32:27 -0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
       <Row>
         <Col md="4" className="footer-copywright" style={{zIndex: 1}}>
           <h3 style={{display: "inline-block", marginRight: "3px"}}>Credit for the initial template: </h3>
-          <a className="purple" href="https://github.com/soumyajit4419/Portfolio" style={{textDecoration: "none", fontWeight: 600}}>soumyajit4419/Portfolio</a>
+          <a target="_blank" rel="noreferrer" className="purple" href="https://github.com/soumyajit4419/Portfolio" style={{textDecoration: "none", fontWeight: 600}}>soumyajit4419/Portfolio</a>
         </Col>
       </Row>
     </Container>
